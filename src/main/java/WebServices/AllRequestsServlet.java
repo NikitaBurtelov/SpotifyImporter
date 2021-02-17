d1004da6950da5a77c8bb1e8aec538b5b21bf9a3
NikitaBurtelov <nikita.burtelov@mail.ru>
2021-02-17 21:17:50 +0300

    fix VkConnector

--- src/main/java/WebServices/AllRequestsServlet.java
+++ src/main/java/WebServices/AllRequestsServlet.java
@@ -1,58 +1,8 @@
-<<<<<<< HEAD
 package WebServices;
 
 import musicdata.Playlist;
 import spotify.SpotifyConnector;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class AllRequestsServlet extends HttpServlet {
-    private WebServer webServer;
-    private Playlist playlist;
-
-
-    public AllRequestsServlet(Playlist playlist) {
-        this.playlist = playlist;
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
-        try{
-            String code = request.getParameter("code");
-
-            response.getWriter().println(code);
-            response.setContentType("text/html;charset=utf-8");
-            response.setStatus(HttpServletResponse.SC_OK);
-            System.out.println(code);
-        }
-        catch (IOException exception) {
-            exception.printStackTrace();
-        }
-    }
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        String url = request.getParameter("url");
-        System.out.println(request.getParameter("url"));
-
-        if (url == null) {
-            response.setContentType("text/html;charset=utf-8");
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-        else {
-            playlist.setUrlVk(url);
-        }
-
-        response.setContentType("text/html;charset=utf-8");
-        System.out.println(url);
-    }
-=======
-package WebServices;
-
-import musicdata.Playlist;
-import spotify.SpotifyConnector;
-import vk.VkСonnector;
+import vk.VkConnector;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -93,11 +43,10 @@ public class AllRequestsServlet extends HttpServlet {
         playlist.setUrl(request.getParameter("url"));
 
         System.out.println("RUN POST VKConn");
-        VkСonnector.setTrackVkPlaylist(playlist, playlist.getUrl());
+        VkConnector.setTrackVkPlaylist(playlist, playlist.getUrl());
 
         spotifyConnector.getCodeUrl();
 
         response.setContentType("text/html;charset=utf-8");
     }
->>>>>>> fix
 }
